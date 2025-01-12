d07bfa7a004770784d82739384032d3cd28763ee
Thomas Bonnefille <thomas.bonnefille@bootlin.com>
2025-01-12 21:44:30 +0100

    Bump gst
    Signed-off-by: Thomas Bonnefille <thomas.bonnefille@bootlin.com>

--- package/gstreamer1/gst1-plugins-bad/gst1-plugins-bad.mk
+++ package/gstreamer1/gst1-plugins-bad/gst1-plugins-bad.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GST1_PLUGINS_BAD_VERSION = 1.22.12
+GST1_PLUGINS_BAD_VERSION = 1.24.11
 GST1_PLUGINS_BAD_SOURCE = gst-plugins-bad-$(GST1_PLUGINS_BAD_VERSION).tar.xz
 GST1_PLUGINS_BAD_SITE = https://gstreamer.freedesktop.org/src/gst-plugins-bad
 GST1_PLUGINS_BAD_INSTALL_STAGING = YES
@@ -49,7 +49,6 @@ GST1_PLUGINS_BAD_CONF_OPTS += \
 	-Dflite=disabled \
 	-Dgs=disabled \
 	-Dgsm=disabled \
-	-Dkate=disabled \
 	-Dladspa=disabled \
 	-Dldac=disabled \
 	-Dlv2=disabled \
